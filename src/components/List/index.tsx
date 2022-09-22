491ee88773f7ecfe00d630f8397bd8ac4e94438f
Antonio Alan <tonio.alan07@gmail.com>
2022-09-21 22:09:55 -0300

    stylization of elements

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import style from './List.module.scss'
 
 function List(){
     const tasks = [{
@@ -9,11 +10,11 @@ function List(){
         time: '01:47:00'
     }]
     return(
-        <aside>
+        <aside className={style.tasksList}>
             <h2>Tasks of the day</h2>
             <ul>
                 {tasks.map((item, index) => (
-                    <li key={index}>
+                    <li key={index} className={style.item}>
                         <h3> {item.task} </h3>
                         <span> {item.time} </span>
                     </li>
